3e38b3c47e629fe88b253f47c6f47a05914d3cd9
MarioAugustoTecnologia <mariopisanibtu@gmail.com>
2024-09-27 09:19:53 -0300

    update

--- server/api/index.js
+++ server/api/index.js
@@ -7,21 +7,6 @@ const cors = require('cors');
 
 server.use(middlewares);
 
-server.use(jsonServer.rewriter({
-    '/api/*': '/$1',
-    '/usuarios/:resource/:id/show': '/:resource/:id',
-    '/vendas/:resource/:id/show': '/:resource/:id',
-    '/atual/:resource/:id/show': '/:resource/:id',
-    '/catproduto/:resource/:id/show': '/:resource/:id',
-    '/produtos/:resource/:id/show': '/:resource/:id',
-    '/despesas/:resource/:id/show': '/:resource/:id',
-    '/compras/:resource/:id/show': '/:resource/:id',
-    '/resultados/:resource/:id/show': '/:resource/:id',
-    '/clientes/:resource/:id/show': '/:resource/:id',
-    '/fornecedor/:resource/:id/show': '/:resource/:id',
-    '/catfornecedor/:resource/:id/show': '/:resource/:id'
-}))
-
 server.use(router);
 
 server.use(cors({origin: ["https://sistemamulticompanyvercel.vercel.app/"], 
